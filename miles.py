9df3aeceaa21544429ec0efcdccacc243c10e62a
Miles Steele <miles@milessteele.com>
2015-12-21 13:47:48 -0500

    update miles

--- miles.py
+++ miles.py
@@ -21,8 +21,6 @@ strip     = Adafruit_DotStar(NUMPIXELS, datapin, clockpin, order="bgr")
 strip.begin()           # Initialize pins for output
 strip.setBrightness(30) # Limit brightness (out of 255)
 
-head = 0
-length = 10
 RED = 0xff0000
 GREEN = 0x00ff00
 BLUE = 0x0000ff
@@ -30,37 +28,56 @@ BLACK = 0x000
 WHITE = 0xffffff
 
 def bound(x):
-        return x % NUMPIXELS
+    return x % NUMPIXELS
 
 class Color(object):
-        @staticmethod
-        def hex_to_rgb(hex_color):
-                return ((hex_color & 0xff0000) >> 16,
-                        (hex_color & 0x00ff00) >> 8,
-                        (hex_color & 0x0000ff))
+    @staticmethod
+    def hex_to_rgb(hex_color):
+        return ((hex_color & 0xff0000) >> 16,
+            (hex_color & 0x00ff00) >> 8,
+            (hex_color & 0x0000ff))
 
-        @staticmethod
-        def rgb_to_hex(r, g, b):
-                return (r << 16) | (g << 8) | b
+    @staticmethod
+    def rgb_to_hex(r, g, b):
+        return (r << 16) | (g << 8) | b
 
-        @staticmethod
-        def hex_to_hsv(hex_color):
-                return colorsys.rgb_to_hsv(*Color.hex_to_rgb(hex_color))
+    @staticmethod
+    def hex_to_hsv(hex_color):
+        return colorsys.rgb_to_hsv(*Color.hex_to_rgb(hex_color))
 
-        @staticmethod
-        def hsv_to_hex(h, s, v):
-                return Color.rgb_to_hex(*map(int, colorsys.hsv_to_rgb(h, s, v)))
+    @staticmethod
+    def hsv_to_hex(h, s, v):
+        return Color.rgb_to_hex(*map(int, colorsys.hsv_to_rgb(h, s, v)))
+
+class Snake(object):
+    def __init__(self, head=0, speed=1):
+        self.head = int(head)
+        self.length = 10
+        self.speed = int(speed)
+        self.hue_offset = head
+
+    def step(self):
+        self.head = bound(self.head + self.speed)
+
+    def show(self):
+        for i in xrange(self.length):
+            h, s, v = ((self.hue_offset+self.head+i)/float(NUMPIXELS)*2.), 1, 255 * (i/float(self.length))
+            strip.setPixelColor(self.head + i, Color.hsv_to_hex(h, s, v))
+
+def strip_clear():
+    for i in xrange(NUMPIXELS):
+        strip.setPixelColor(i, BLACK)
+
+N_SNAKES = 10
+snakes = [Snake(head=i*(NUMPIXELS / float(N_SNAKES)), speed=1+(0.3*i)) for i in xrange(N_SNAKES)]
 
 while True:
-        strip.show()
-        time.sleep(1.0 / 60)
-        print Color.hex_to_hsv(GREEN)
+    strip.show()
+    time.sleep(1.0 / 60)
+    print Color.hex_to_hsv(GREEN)
 
-        for i in xrange(NUMPIXELS):
-                strip.setPixelColor(i, BLACK)
+    strip_clear()
 
-        for i in xrange(length):
-                h, s, v = ((head+i)/float(NUMPIXELS)*2.), 1, 255 * (i/float(length))
-                strip.setPixelColor(head + i, Color.hsv_to_hex(h, s, v))
-
-        head = bound(head + 1)
+    for snake in snakes:
+        snake.show()
+        snake.step()
