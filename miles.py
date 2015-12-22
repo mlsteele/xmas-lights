ebe1b1507a4c4e8088dd5670485c27cd228d558b
Miles Steele <miles@milessteele.com>
2015-12-22 16:54:15 -0500

    add angle calculations

--- miles.py
+++ miles.py
@@ -3,6 +3,7 @@
 import time
 import colorsys
 import random
+from math import sin, cos
 from dotstar import Adafruit_DotStar
 
 NUMPIXELS = 900 # Number of LEDs in strip
@@ -20,6 +21,65 @@ GREEN = 0x00ff00
 BLUE  = 0x0000ff
 BLACK = 0x000000
 WHITE = 0xffffff
+
+class PixelAngle(object):
+    # Map from pixel indices to angles in degrees.
+    # The front face of the tree is 0.
+    # Angles increase CCW looking from the heavens.
+    REF_ANGLES = {
+        0: 180,
+        120: 0,
+        336: 0,
+        527: 0,
+        648: 0,
+        737: 0,
+        814: 0,
+        862: 0,
+        876: 0,
+        886: 0,
+        NUMPIXELS: 180,
+    }
+
+    cache = {}
+
+    @staticmethod
+    def angle(i):
+        """Get the angle of a pixel.
+        Estimate by linear approximation between two closest known neighbors.
+        """
+        if i in PixelAngle.cache:
+            return PixelAngle.cache[i]
+
+        if i in PixelAngle.REF_ANGLES.keys():
+            return float(PixelAngle.REF_ANGLES[i])
+
+        left_i, right_i = PixelAngle.closest(i)
+        left_a, right_a = PixelAngle.REF_ANGLES[left_i], PixelAngle.REF_ANGLES[right_i]
+        if right_a <= left_a:
+            right_a += 360
+        ratio = (i - left_i) / float(right_i - left_i)
+        predicted = (left_a * (1 - ratio) + right_a * ratio)
+        predicted %= 360
+
+        PixelAngle.cache[i] = predicted
+        return predicted
+
+    @staticmethod
+    def closest(i):
+        """Get the closest known neighbor(s) of a pixel."""
+        items = PixelAngle.REF_ANGLES.keys()
+        assert i not in items
+        lesser = [x for x in items if x < i]
+        greater = [x for x in items if x > i]
+        left_i = sorted(lesser, key=lambda x: abs(x - i))[0]
+        right_i = sorted(greater, key=lambda x: abs(x - i))[0]
+        return (left_i, right_i)
+
+def angdist(x, y):
+    """Minimum distance between two angles (in degrees)."""
+    x = (x + 360) % 360
+    y = (y + 360) % 360
+    return min([abs((x - y) % 360), abs((y - x) % 360)])
 
 def bound(x):
     return x % NUMPIXELS
@@ -86,6 +146,18 @@ class Sparkle(object):
             if random.random() > 0.999:
                 strip.setPixelColor(i, Color.hsv_to_hex(random.random(), 0.3, random.random()))
 
+class Predicate(object):
+    def __init__(self, predicate):
+        self.f = predicate
+
+    def step(self):
+        pass
+
+    def show(self):
+        for i in xrange(NUMPIXELS):
+            if self.f(i):
+                strip.setPixelColor(i, Color.hsv_to_hex(0, 0, 0.04))
+
 def strip_clear():
     for i in xrange(NUMPIXELS):
         strip.setPixelColor(i, BLACK)
@@ -98,6 +170,14 @@ sprites.append(EveryNth(factor=0.1))
 sprites.append(EveryNth(factor=0.1, speed=-0.2))
 sprites.append(Sparkle())
 
+# Playing with angles.
+# angle_offset = lambda: time.time() * 45 % 360
+# angle_offset = lambda: sin(time.time()) * 55
+# angle_width = lambda: 10
+# sprites.append(Predicate(lambda x: angdist(PixelAngle.angle(x), angle_offset()) <= angle_width()))
+
+print "Starting."
+
 last_frame_t = time.time()
 ideal_frame_delta_t = 1.0 / 60
 while True:
@@ -105,6 +185,8 @@ while True:
     delta_t = frame_t - last_frame_t
     if delta_t < ideal_frame_delta_t:
         time.sleep(ideal_frame_delta_t - delta_t)
+    # else:
+    #     print "Frame lagging. Time to optimize."
     last_frame_t = time.time()
 
     strip_clear()
