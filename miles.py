8b09396481183a36abf8d06bf0caef12b90427df
Miles Steele <miles@milessteele.com>
2015-12-22 18:15:59 -0500

    add profiler

--- miles.py
+++ miles.py
@@ -84,6 +84,21 @@ def angdist(x, y):
 def bound(x):
     return x % NUMPIXELS
 
+class Profiler:
+    def __init__(self, name="_"):
+        self.name = name
+
+    # Thanks to:
+    # http://preshing.com/20110924/timing-your-code-using-pythons-with-statement/
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.interval = self.end - self.start
+        print "profiled {}: {:.0f} (ms)".format(self.name, self.interval * 1000)
+
 class Color(object):
     @staticmethod
     def hex_to_rgb(hex_color):
