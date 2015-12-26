6a15fb36182a3b59fdcbe6ec92719d8f36c13603
Oliver Steele <steele@osteele.com>
2015-12-25 21:21:19 -0500

    check the Slack token

--- webserver.py
+++ webserver.py
@@ -1,9 +1,12 @@
-import os
+import os, re
 import flask
 from flask import Flask, abort, request
+from flask import abort
 from messages import publish
 
 app = Flask(__name__)
+
+SLACK_WEBHOOK_TOKEN = os.environ.get('SLACK_WEBHOOK_TOKEN')
 
 @app.route('/ifttt', methods=['POST'])
 def ifttt():
@@ -12,6 +15,8 @@ def ifttt():
 
 @app.route('/slack', methods=['POST'])
 def slack():
+    if SLACK_WEBHOOK_TOKEN and SLACK_WEBHOOK_TOKEN != request.form['token']:
+        return abort(403)
     message = request.form['text']
     message = re.sub(r'[!@]\S+\s*', '', message)
     publish(message)
