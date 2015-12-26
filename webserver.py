57db21e21f91226b01a0976d3c149331c44481d8
Miles Steele <miles@milessteele.com>
2015-12-26 13:06:11 -0500

    merge gamepad server into webserver

--- webserver.py
+++ webserver.py
@@ -1,15 +1,16 @@
 import logging, os, re, sys
 import flask
-from flask import Flask, abort, request
-from flask import abort
+from flask import Flask, abort, request, send_from_directory, abort
+from flask.ext.socketio import SocketIO, emit
 
-from messages import publish
+import messages
 logging.getLogger('messages').setLevel(logging.INFO)
 
 IFTTT_TOKEN = os.environ.get('IFTTT_TOKEN')
 SLACK_WEBHOOK_TOKEN = os.environ.get('SLACK_WEBHOOK_TOKEN')
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 import logging
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -19,7 +20,7 @@ app.logger.setLevel(logging.DEBUG)
 def ifttt():
     if IFTTT_TOKEN and IFTTT_TOKEN != request.form['token']:
         return abort(403)
-    publish('action', action=request.form['action'])
+    messages.publish('action', action=request.form['action'])
     return 'ok'
 
 @app.route('/slack', methods=['POST'])
@@ -28,8 +29,21 @@ def slack():
         return abort(403)
     message = request.form['text']
     message = re.sub(r'[!@]\S+\s*', '', message)
-    publish('action', action=message)
+    messages.publish('action', action=message)
     return flask.jsonify(text='ok')
 
+@app.route("/")
+def index():
+    return send_from_directory("static", "index.html")
+
+@socketio.on("ctl", namespace="/ctl")
+def ctl_message(message):
+    payload = {
+        "key": message.get("key"),
+        "state": bool(message.get("state")),
+    }
+    print payload
+    messages.publish("gamekey", **payload)
+
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, use_reloader=True)
