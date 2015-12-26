ae97ec22767d6e66361c2bb46ad50a254d37dff3
Miles Steele <miles@milessteele.com>
2015-12-26 01:29:15 -0500

    add gamepad visuals

--- gamepad/static/main.js
+++ gamepad/static/main.js
@@ -1,7 +1,31 @@
 $(document).ready(function(){
-    var socket = io.connect("http://" + document.domain + ":" + location.port + "/ctl");
-    socket.on("connect", function() {
-	console.log("Socket connected.")
-	socket.emit("ctl", {"key": "right", "state": true});
-    });
+  var socket = io.connect("http://" + document.domain + ":" + location.port + "/ctl");
+  socket.on("connect", function() {
+    console.log("Socket connected.")
+  });
+
+  function emit_update(key, state) {
+    socket.emit("ctl", {"key": key, "state": state});
+  }
+
+  var btn_left = $(".btn-left");
+  var btn_right = $(".btn-right");
+
+  btn_left.on("touchstart", function(e) {
+    e.preventDefault();
+    emit_update("left", true);
+  })
+  btn_left.on("touchend touchcancel", function(e) {
+    e.preventDefault();
+    emit_update("left", false);
+  });
+
+  btn_right.on("touchstart", function(e) {
+    e.preventDefault();
+    emit_update("right", true);
+  })
+  btn_right.on("touchend touchcancel", function(e) {
+    e.preventDefault();
+    emit_update("right", false);
+  });
 });
