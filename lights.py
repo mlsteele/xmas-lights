02079ce3e7991ab8d40cdc5e5f2fa07220afc9ec
Oliver Steele <steele@osteele.com>
2015-12-28 10:48:07 -0500

    extract NUMPIXELS, PixelAngle to new file

--- lights.py
+++ lights.py
@@ -6,63 +6,9 @@ import signal
 from math import sin, cos, pi
 import apa102
 from messages import get_message
-
-NUMPIXELS = 900 # Number of LEDs in strip
+from light_geometry import NUMPIXELS, PixelAngle
 
 strip = apa102.APA102(NUMPIXELS)
-
-class PixelAngle(object):
-    # Map from pixel indices to angles in degrees.
-    # The front face of the tree is 0.
-    # Angles increase CCW looking from the heavens.
-    REF_ANGLES = {
-        0: 180,
-        120: 0,
-        336: 0,
-        527: 0,
-        648: 0,
-        737: 0,
-        814: 0,
-        862: 0,
-        876: 0,
-        886: 0,
-        NUMPIXELS: 180,
-    }
-
-    cache = {}
-
-    @staticmethod
-    def angle(i):
-        """Get the angle of a pixel.
-        Estimate by linear approximation between two closest known neighbors.
-        """
-        if i in PixelAngle.cache:
-            return PixelAngle.cache[i]
-
-        if i in PixelAngle.REF_ANGLES.keys():
-            return float(PixelAngle.REF_ANGLES[i])
-
-        left_i, right_i = PixelAngle.closest(i)
-        left_a, right_a = PixelAngle.REF_ANGLES[left_i], PixelAngle.REF_ANGLES[right_i]
-        if right_a <= left_a:
-            right_a += 360
-        ratio = (i - left_i) / float(right_i - left_i)
-        predicted = (left_a * (1 - ratio) + right_a * ratio)
-        predicted %= 360
-
-        PixelAngle.cache[i] = predicted
-        return predicted
-
-    @staticmethod
-    def closest(i):
-        """Get the closest known neighbor(s) of a pixel."""
-        items = PixelAngle.REF_ANGLES.keys()
-        assert i not in items
-        lesser = [x for x in items if x < i]
-        greater = [x for x in items if x > i]
-        left_i = sorted(lesser, key=lambda x: abs(x - i))[0]
-        right_i = sorted(greater, key=lambda x: abs(x - i))[0]
-        return (left_i, right_i)
 
 def angdist(x, y):
     """Minimum distance between two angles (in degrees)."""
