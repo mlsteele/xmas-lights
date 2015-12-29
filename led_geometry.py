dbca8b83bc4547584e8d1d47517254dd1cc26b99
Oliver Steele <steele@osteele.com>
2015-12-28 19:27:31 -0500

    simplify PixelAngle.angle

--- led_geometry.py
+++ led_geometry.py
@@ -62,6 +62,7 @@ class PixelAngle(object):
     # Angles increase CCW looking from the heavens.
     REF_ANGLES = {}
     for angle, indices in CONFIG['pixels']['angles'].items():
+        angle = float(angle)
         for index in indices:
             REF_ANGLES[index] = angle
 
@@ -72,30 +73,22 @@ class PixelAngle(object):
         """Get the angle of a pixel.
         Estimate by linear approximation between two closest known neighbors.
         """
-        if i in PixelAngle.cache:
-            return PixelAngle.cache[i]
+        angle = PixelAngle.cache.get(i)
+        if angle is not None:
+            return angle
 
-        if i in PixelAngle.REF_ANGLES.keys():
-            return float(PixelAngle.REF_ANGLES[i])
+        angle = PixelAngle.REF_ANGLES.get(i)
+        if angle is not None:
+            return angle
 
-        left_i, right_i = PixelAngle.closest(i)
+        keys = PixelAngle.REF_ANGLES.keys()
+        left_i = max(x for x in keys if x < i)
+        right_i = min(x for x in keys if i < x)
         left_a, right_a = PixelAngle.REF_ANGLES[left_i], PixelAngle.REF_ANGLES[right_i]
         if right_a <= left_a:
             right_a += 360
         ratio = (i - left_i) / float(right_i - left_i)
-        predicted = (left_a * (1 - ratio) + right_a * ratio)
-        predicted %= 360
+        predicted = (left_a * (1 - ratio) + right_a * ratio) % 360
 
         PixelAngle.cache[i] = predicted
         return predicted
-
-    @staticmethod
-    def closest(i):
-        """Get the closest known neighbor(s) of a pixel."""
-        items = PixelAngle.REF_ANGLES.keys()
-        assert i not in items
-        lesser = [x for x in items if x < i]
-        greater = [x for x in items if x > i]
-        left_i = sorted(lesser, key=lambda x: abs(x - i))[0]
-        right_i = sorted(greater, key=lambda x: abs(x - i))[0]
-        return (left_i, right_i)
