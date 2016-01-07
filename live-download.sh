e62e592fc9daab2289afe517046bd5e9bc2d8f95
Oliver Steele <steele@osteele.com>
2016-01-07 08:51:33 -0500

    teach --hostname to  live-download.sh

--- live-download.sh
+++ live-download.sh
@@ -1,4 +1,24 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -eu
+
+die() { echo "$@" 1>&2; exit 1; }
+
+USERNAME=pi
+HOSTNAME=xmas-pi
+
+while test $# -gt 0
+do
+  case "$1" in
+    -h|--hostname|--host)
+      shift
+      HOSTNAME="$1"
+      ;;
+
+    *)
+      die "error: unknown argument $1"
+      ;;
+  esac
+  shift
+done
 
 cd `dirname $0`
-find . -name \*.py | entr -r rsync -aizF . pi@xmas-pi:xmas-lights
+find . -name \*.py | entr -r rsync -aizF . ${USERNAME}@${HOSTNAME}:xmas-lights
